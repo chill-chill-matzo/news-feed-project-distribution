94d78999b8301e8c8f5f1b40bfcb286d65d83cf5
heeyeon <godeeyeon@gmail.com>
2023-06-26 16:27:31 +0900

    chore. 기본적인 폴더 구조 및 공통 환경 세팅
    Co-authored-by: Yoonseo Choi, 최윤서 <oiooeo@users.noreply.github.com>
    Co-authored-by: Chae hyun Kim <chaehyun09@users.noreply.github.com>
    Co-authored-by: Hwang Tae Gyu <GHGT@users.noreply.github.com>
    Co-authored-by: orasio3477 <orasio3477@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import './App.css';
+import GlobalStyle from './GlobalStyle';
+import Router from './shared/Router';
 
 function App() {
-  return <div>app</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
 
 export default App;
---
+++ src/shared/Router.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from '../pages/Main';
+import Detail from '../pages/Detail';
+// import MyPage from '../pages/MyPage';
+import Layout from './Layout';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="detail" element={<Detail />} />
+          {/* <Route path="mypage" element={<MyPage />} /> */}
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
