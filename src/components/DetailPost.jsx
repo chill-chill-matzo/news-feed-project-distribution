59878e541c95fc030fcc1567ad20e24853ec3bfd
김채현 <kimchennie@naver.com>
2023-06-28 09:21:34 +0900

    디테일페이지 사진 가운데정렬

--- src/components/DetailPost.jsx
+++ src/components/DetailPost.jsx
@@ -1,55 +1,55 @@
-import React from 'react'
-import { styled } from 'styled-components'
+import React from 'react';
+import { styled } from 'styled-components';
 import { BlueButton } from '../shared/Buttons';
 
 function DetailPost() {
   return (
     <>
-    <DetailImage/>
-    <DetailContainer>
-      <DetailTitle>제목</DetailTitle>
-      <DetailBody>상세 내용.....상세 내용.....상세 내용.....상세 내용.....상세 내용.....
-        상세 내용..... 상세 내용.....상세 내용.....상세 내용.....상세 내용.....상세 내용.....
-        상세 내용.....나는 뭉충이이
-        저만 알고 싶은 맛집입니다.. 알려드려요.. 고맙죠?
-      </DetailBody>
-      <ButtonSet>
-        <BlueButton>수정</BlueButton>
-        <BlueButton>삭제</BlueButton>
-      </ButtonSet>
-    </DetailContainer>
+      <DetailImage />
+      <DetailContainer>
+        <h1>제목</h1>
+        <p>
+          상세 내용.....상세 내용.....상세 내용.....상세 내용.....상세 내용..... 상세 내용..... 상세 내용.....상세
+          내용.....상세 내용.....상세 내용.....상세 내용..... 상세 내용.....나는 뭉충이이 저만 알고 싶은 맛집입니다..
+          알려드려요.. 고맙죠?
+        </p>
+        <ButtonSet>
+          <BlueButton>수정</BlueButton>
+          <BlueButton>삭제</BlueButton>
+        </ButtonSet>
+      </DetailContainer>
     </>
-  )
+  );
 }
 
-export default DetailPost
+export default DetailPost;
 
 const DetailImage = styled.div`
-background-image: url('https://i.pinimg.com/564x/68/4c/ed/684ced199400f4316b10a9083a37a0f0.jpg');
-background-size: 100%;
-background-position: center;
-width: 90%;
-height: 500px;
-position: relative;
+  position: relative;
+  width: 90%;
+  height: 500px;
+  margin: 30px auto;
+  background-image: url('https://i.pinimg.com/564x/68/4c/ed/684ced199400f4316b10a9083a37a0f0.jpg');
+  background-size: 100%;
+  background-position: center;
 `;
 
 const DetailContainer = styled.div`
   width: 87%;
-  margin: 30px auto 30px;
-`
+  margin: 20px auto 20px;
 
-const DetailTitle = styled.h1`
-  font-size: 25px;
-  font-weight: bold;
-  
-`
+  h1 {
+    font-size: 25px;
+    font-weight: bold;
+  }
 
-const DetailBody = styled.div`
-  padding: 10px;
-  font-size: 17px;
-`
+  p {
+    margin-top: 15px;
+    font-size: 17px;
+  }
+`;
 
 const ButtonSet = styled.div`
   display: flex;
   justify-content: right;
-`
+`;
