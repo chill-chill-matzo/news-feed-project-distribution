cd07d74fc613f2e6ca7da71a0a6c3c0e01962fbc
heeyeon <godeeyeon@gmail.com>
2023-06-28 12:54:26 +0900

    feat. 새글 작성 페이지 firebase에 데이터 연결
    Co-authored-by: Yoonseo Choi, 최윤서 <oiooeo@users.noreply.github.com>
    Co-authored-by: Chae hyun Kim <chaehyun09@users.noreply.github.com>

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -2,42 +2,88 @@
 import { TitleInput, ContentInput } from '../shared/Input';
 import { BlueButton } from '../shared/Buttons';
 import { styled } from 'styled-components';
+import { addDoc, collection, getDocs, query, doc } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useEffect, useState } from 'react';
 
-import { TextButton } from '../shared/Buttons';
-import { useNavigate } from 'react-router-dom';
+const AddPost = () => {
+  const [postStorage, setPostStorage] = useState([]);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [like, setLike] = useState(0);
+  const [image, setImage] = useState('');
+  const [user, setUser] = useState('');
+  const [time, setTime] = useState('');
 
-function AddPost() {
-  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchData = async () => {
+      const q = query(collection(db, 'PostStorage'));
+      const querySnapshot = await getDocs(q);
+
+      const initialPostStorage = [];
+
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        const data = {
+          id: doc.id,
+          ...doc.data()
+        };
+        console.log(data);
+        initialPostStorage.push(data);
+      });
+      setPostStorage(initialPostStorage);
+    };
+    fetchData();
+  }, []);
+
+  const onChange = (event) => {
+    const {
+      target: { name, value }
+    } = event;
+    if (name === 'title') {
+      setTitle(value);
+    }
+    if (name === 'content') {
+      setContent(value);
+    }
+  };
+
+  const addNewPost = async (event) => {
+    event.preventDefault();
+
+    // setTime
+    // setUser
+
+    const collectionRef = collection(db, 'PostStorage');
+    const newPost = { title, content, like, image, time, user };
+
+    await addDoc(collectionRef, newPost);
+
+    setPostStorage((prev) => {
+      return [...postStorage, newPost];
+    });
+    setTitle('');
+    setContent('');
+  };
+
   return (
     <Div>
-      <StAside>
-        <TextButton
-          onClick={() => {
-            navigate('mypage');
-          }}
-        >
-          마이페이지
-        </TextButton>
-
-        <TextButton>로그아웃</TextButton>
-      </StAside>
-
       <p>여러분의 맛집을 추천해주세요!</p>
       <Container>
-        <TitleInput />
-        <ContentInput />
+        <TitleInput name="title" value={title} onChange={onChange} />
+        <ContentInput name="content" value={content} onChange={onChange} />
         <View></View>
         <ButtonsContainer>
           <BlueButton>사진 선택</BlueButton>
           <div>
             <BlueButton>취소</BlueButton>
-            <BlueButton>등록</BlueButton>
+            <BlueButton onClick={addNewPost}>등록</BlueButton>
           </div>
         </ButtonsContainer>
       </Container>
     </Div>
   );
-}
+};
 
 export default AddPost;
 
